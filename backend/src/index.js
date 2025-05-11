04db2e07f1cfdf4adb62db1e68db4257e42ba725
Heniloza <ozahenil73@gmail.com>
2025-05-11 10:45:36 +0530

    production ready changes done

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,12 +6,15 @@ dotenv.config();
 import authRoutes from "./routes/authRoutes.js";
 import messageRoutes from "./routes/messageRoutes.js";
 import connectMongoDb from "./dbConnection.js";
-import { app, server, io } from "./lib/socket.js";
+import { app, server } from "./lib/socket.js";
+import path from "path";
 
 //Database connection
 connectMongoDb(process.env.MONGO_URL)
   .then(() => console.log("CONNECTED TO MONGODB SUCCESSFULLY"))
   .catch((err) => console.log("error in connecting to database", err));
+
+const __dirname = path.resolve();
 
 //Middleware
 app.use(
@@ -27,6 +30,14 @@ app.use(epxress.json());
 //Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(epxress.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 server.listen(process.env.PORT, () =>
   console.log(`SERVER + SOCKET.IO STARTED AT PORT ${process.env.PORT}`)
